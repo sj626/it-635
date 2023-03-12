24bc715602d940bc03b3079639b75af53d0277e5
Shola Jolaosho <45631237+sj626@users.noreply.github.com>
2023-03-12 00:32:13 -0500

    Update output.py

--- output.py
+++ output.py
@@ -14,58 +14,49 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 # Execute a SELECT statement to retrieve the data
-cur.execute("SELECT * FROM employees")
+cur.execute("SELECT * FROM assure.employees")
 employees = cur.fetchall()
 
-cur.execute("SELECT * FROM suppliers")
-suppliers = cur.fetchall()
-
-cur.execute("SELECT * FROM manufacturers")
-manufacturers = cur.fetchall()
-
-#cur.execute("SELECT * FROM items")
-#items = cur.fetchall()
-
-cur.execute("SELECT * FROM inventory")
+cur.execute("SELECT * FROM assure.inventory")
 inventory = cur.fetchall()
+
+cur.execute("SELECT * FROM assure.funds")
+funds = cur.fetchall()
+
+cur.execute("SELECT * FROM assure.time_records")
+time_records = cur.fetchall()
 
 # Print the results
 print("Employees:")
 for employee in employees:
     print(employee)
 
-print("Suppliers:")
-for supplier in suppliers:
-    print(supplier)
-
-print("Manufacturers:")
-for manufacturer in manufacturers:
-    print(manufacturer)
-
-#print("Items:")
-#for item in items:
- #   print(item)
-
 print("Inventory:")
 for item in inventory:
     print(item)
 
+print("Funds:")
+for fund in funds:
+    print(fund)
+
+print("Time Records:")
+for record in time_records:
+    print(record)
+
 # Convert the data to a Pandas DataFrame
-df_employees = pd.DataFrame(employees, columns=['employee_id', 'first_name', 'last_name', 'email', 'phone_number', 'street_address', 'city', 'state', 'zip_code', 'hire_date'])
-df_suppliers = pd.DataFrame(suppliers, columns=['supplier_id', 'name', 'street_address', 'city', 'state', 'contact_name', 'phone_number'])
-df_manufacturers = pd.DataFrame(manufacturers, columns=['manufacturer_id', 'name', 'street_address', 'city', 'state', 'zip', 'phone'])
-#df_items = pd.DataFrame(items, columns=['item_id', 'name', 'description', 'manufacturer_id'])
-df_inventory = pd.DataFrame(inventory, columns=['inventory_id', 'item_id', 'quantity', 'price', 'supplier_id', 'date_added'])
+df_employees = pd.DataFrame(employees, columns=['employee_id', 'hourly_rate', 'employee_name'])
+df_inventory = pd.DataFrame(inventory, columns=['inventory_id', 'item_name', 'quantity', 'price'])
+df_funds = pd.DataFrame(funds, columns=['funds_id', 'amount'])
+df_time_records = pd.DataFrame(time_records, columns=['record_id', 'employee_id', 'start_time', 'end_time'])
 
 # Write the data to an Excel file
 with pd.ExcelWriter('assure_data.xlsx') as writer:
     df_employees.to_excel(writer, sheet_name='employees', index=False)
-    df_suppliers.to_excel(writer, sheet_name='suppliers', index=False)
-    df_manufacturers.to_excel(writer, sheet_name='manufacturers', index=False)
-    #df_items.to_excel(writer, sheet_name='items', index=False)
     df_inventory.to_excel(writer, sheet_name='inventory', index=False)
+    df_funds.to_excel(writer, sheet_name='funds', index=False)
+    df_time_records.to_excel(writer, sheet_name='time_records', index=False)
 
 # Close the cursor and database connection
 cur.close()
 conn.close()
-                                                       
+   
