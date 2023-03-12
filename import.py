c5c454cae54846d825d07673039f5bbdcb0f6755
Shola Jolaosho <45631237+sj626@users.noreply.github.com>
2023-03-11 23:22:06 -0500

    Update import.py

--- import.py
+++ import.py
@@ -4,60 +4,28 @@ import psycopg2
 conn = psycopg2.connect("postgresql://assure:testing@localhost/assure")
 cur = conn.cursor()
 
-# create tables if they do not exist
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS employees (
-        id SERIAL PRIMARY KEY,
-        first_name VARCHAR(255),
-        last_name VARCHAR(255),
-        email VARCHAR(255),
-        phone VARCHAR(255)
-    )
-""")
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS suppliers (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255),
-        email VARCHAR(255),
-        phone VARCHAR(255)
-    )
-""")
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS manufacturers (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255),
-        email VARCHAR(255),
-        phone VARCHAR(255)
-    )
-""")
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS inventory (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(255),
-        description TEXT,
-        manufacturer_id INTEGER REFERENCES manufacturers(id)
-    )
-""")
-
 # insert employee data
 while True:
-    print("Please enter employee first name")
-    emp_first_name = input()
-    print("Please enter employee last name")
-    emp_last_name = input()
-    print("Please enter employee email")
-    emp_email = input()
-    print("Please enter employee phone number")
-    emp_phone = input()
-    # Assuming there's an "employees" table with an "id" column as primary key
-cur.execute("""
-    INSERT INTO employees (employee_id, first_name, last_name, email, phone)
-    VALUES (%s, %s, %s, %s, %s)
-    RETURNING id;
-""", (employee_id, first_name, last_name, email, phone))
+    print("Please enter employee name")
+    emp_name = input()
+    print("Please enter hourly rate")
+    emp_hourly_rate = input()
+    print("PLease enter employee id")
+    emp_id = input()
+    cur.execute("""
+        INSERT INTO employees (employee_id, hourly_rate, employee_name)
+        VALUES (%s, %s, %s)
+        RETURNING employee_id;
+    """, (emp_id, emp_hourly_rate, emp_name))
 
-emp_id = cur.fetchone()[0]
-print(f"Employee submitted: ID {emp_id}")
+    emp_id = cur.fetchone()[0]
+    print(f"Employee submitted: ID {emp_id}")
+
+    # Ask user if they want to enter another employee
+    print("Do you want to enter another employee? (y/n)")
+    response = input().lower()
+    if response != "y":
+        break
 
 # close database connection
 cur.close()
